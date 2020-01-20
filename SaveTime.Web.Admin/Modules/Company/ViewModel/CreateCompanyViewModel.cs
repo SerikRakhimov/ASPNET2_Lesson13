da1e5d5d5ad5210797e853350140a86dec6d23df
RSB <s_astana@mail.ru>
2020-01-20 12:18:57 +0600

    first commit

---
+++ SaveTime.Web.Admin/Modules/Company/ViewModel/CreateCompanyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaveTime.Web.Admin.Modules.Company.ViewModel
+{
+    public class CreateCompanyViewModel
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
